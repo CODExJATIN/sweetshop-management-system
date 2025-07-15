4b5214e403c1766c9378a7a8a044582839a87454
codexjatin <jjatinparmar54@gmail.com>
2025-07-16 00:53:56 +0530

    feat: implement getAllSweets controller and route to pass test

--- server/routes/sweets.js
+++ server/routes/sweets.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addSweet } = require('../controllers/sweetController');
+const { addSweet,getAllSweets } = require('../controllers/sweetController');
 
 // Add Sweet
 router.post('/', addSweet);
+router.get('/',getAllSweets);
 
 module.exports = router;
