3f43466d3e8edcccf137d1152a0f0bea4ebcd44f
codexjatin <jjatinparmar54@gmail.com>
2025-07-16 23:29:23 +0530

    added searchSweets controller with query validation and fixed matching test case

--- server/routes/sweets.js
+++ server/routes/sweets.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addSweet,getAllSweets,deleteSweetById } = require('../controllers/sweetController');
+const { addSweet,getAllSweets,deleteSweetById, searchSweets } = require('../controllers/sweetController');
 
 // Add Sweet
 router.post('/', addSweet);
 router.get('/',getAllSweets);
 router.delete('/:id', deleteSweetById);
+router.get('/search', searchSweets);
 
 module.exports = router;
