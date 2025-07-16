22b4ad7f59e0116e19cebc12ab71b3c29b1a7e7f
codexjatin <jjatinparmar54@gmail.com>
2025-07-16 22:54:50 +0530

    added deleteSweetById controller and route to pass tests

--- server/controllers/sweetController.js
+++ server/controllers/sweetController.js
@@ -1,4 +1,5 @@
 const Sweet = require('../models/Sweet');
+const mongoose = require('mongoose');
 
 // POST /sweets
 exports.addSweet = async (req, res) => {
@@ -58,3 +59,39 @@ exports.getAllSweets = async (req, res) => {
     });
   }
 };
+
+exports.deleteSweetById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check for valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid ID format.',
+      });
+    }
+
+    const deletedSweet = await Sweet.findByIdAndDelete(id);
+
+    if (!deletedSweet) {
+      return res.status(404).json({
+        success: false,
+        message: 'Sweet not found.',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Sweet deleted successfully.',
+    });
+  } catch (err) {
+    console.error('Error deleting sweet:', err.message);
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error: err.message,
+    });
+  }
+};
+
--- server/routes/sweets.js
+++ server/routes/sweets.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addSweet,getAllSweets } = require('../controllers/sweetController');
+const { addSweet,getAllSweets,deleteSweetById } = require('../controllers/sweetController');
 
 // Add Sweet
 router.post('/', addSweet);
 router.get('/',getAllSweets);
+router.delete('/:id', deleteSweetById);
 
 module.exports = router;
